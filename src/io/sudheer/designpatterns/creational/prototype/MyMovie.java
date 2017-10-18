ca814cf25a04a37ed5f429c3bd9cfa0ea15512c0
veersudhir83 <veersudhir83@gmail.com>
2017-10-18 21:07:41 +0530

    Created Own MyRegistry to demonstrate prototype

---
+++ src/io/sudheer/designpatterns/creational/prototype/MyMovie.java
@@ -0,0 +1,15 @@
+package io.sudheer.designpatterns.creational.prototype;
+
+public class MyMovie extends MyItem {
+
+	private String runtime;
+
+	public String getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
+	
+}
---
+++ src/io/sudheer/designpatterns/creational/prototype/MyRegistryDemo.java
@@ -0,0 +1,24 @@
+package io.sudheer.designpatterns.creational.prototype;
+
+public class MyRegistryDemo {
+
+	public static void main(String[] args) {
+
+		MyRegistry registry = new MyRegistry();
+		MyMovie movie = (MyMovie) registry.createItem("Movie");
+		movie.setTitle("Harry Potter");
+		
+		System.out.println(movie);
+		System.out.println(movie.getTitle());
+		System.out.println(movie.getUrl());
+		
+		MyBook book = (MyBook) registry.createItem("Book");
+		book.setTitle("Design Patterns");
+		
+		System.out.println(book);
+		System.out.println(book.getTitle());
+		System.out.println(book.getUrl());
+		
+	}
+
+}
